e18cf16709c6da736b35a19d4f760895178e3e9d
SamoelDuarte <samoelduarte@betasolucao.com.br>
2025-10-22 14:45:19 -0300

    Update ApiMockController.php

--- app/Http/Controllers/ApiMockController.php
+++ app/Http/Controllers/ApiMockController.php
@@ -37,45 +37,44 @@ class ApiMockController extends Controller
     }
     function gerarToken()
     {
-        $clientId = env('HAVAN_API_CLIENT_ID');
-        $username = env('HAVAN_API_USERNAME');
-        $password = env('HAVAN_API_PASSWORD');
-
-        $postFields = http_build_query([
-            'grant_type' => 'password',
-            'client_id' => $clientId,
-            'username' => $username,
-            'password' => $password
-        ]);
-
+        // Inicializa a sessão cURL
         $curl = curl_init();
+
+        // Configurações da requisição cURL
         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://cobrancaexternaauthapi.apps.havan.com.br/token',
-            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_URL => 'https://cobrancaexternaauthapi.apps.havan.com.br/token', // URL do endpoint de autenticação
+            CURLOPT_RETURNTRANSFER => true, // Retorna a resposta como string
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
             CURLOPT_TIMEOUT => 0,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => $postFields,
+            CURLOPT_CUSTOMREQUEST => 'POST', // Tipo da requisição
+            CURLOPT_POSTFIELDS => 'grant_type=password&client_id=bd210e1b-dac2-49b0-a9c4-7c5e1b0b241f&username=THF&password=3cr1O35JfhQ8vBO', // Parâmetros do POST
             CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/x-www-form-urlencoded'
+                'Content-Type: application/x-www-form-urlencoded' // Tipo de conteúdo
             ),
         ));
 
+        // Executa a requisição e captura a resposta
         $response = curl_exec($curl);
+
+        // Verifica se ocorreu erro na requisição cURL
         if ($response === false) {
             echo 'Erro cURL: ' . curl_error($curl);
             return null;
         }
+
+        // Fecha a sessão cURL
         curl_close($curl);
 
+        // Converte a resposta JSON para um array PHP
         $responseData = json_decode($response, true);
+        // Verifica se a resposta contém o token
         if (isset($responseData['access_token'])) {
             return $responseData['access_token'];
         } else {
-            echo 'Erro ao obter o token: ' . json_encode($responseData);
+            echo 'Erro ao obter o token: ' . $responseData;
             return null;
         }
     }
